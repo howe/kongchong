058381888ab2e20f5d8bc5f174d3786e1f59122b
Howe Chiang <howechiang@gmail.com>
2012-08-14 17:35:48 +0800

    初始化

--- src/com/esup/kongchong/module/StocksModule.java
+++ src/com/esup/kongchong/module/StocksModule.java
@@ -1,12 +1,11 @@
 package com.esup.kongchong.module;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.nutz.dao.pager.Pager;
+import org.nutz.dao.Dao;
+import org.nutz.dao.Sqls;
+import org.nutz.dao.sql.Sql;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.mvc.annotation.At;
+import org.nutz.mvc.annotation.Ok;
 import org.nutz.mvc.annotation.Param;
 import org.nutz.service.EntityService;
 
@@ -14,61 +13,88 @@ import org.nutz.log.Log;
 import org.nutz.log.Logs;
 
 import com.esup.kongchong.bean.Order;
+import com.esup.kongchong.bean.Stocks;
 
-@At("/Stocks")
-@IocBean(fields={"dao"})
-public class StocksModule extends EntityService<Order>{
+@IocBean(fields = { "dao" })
+public class StocksModule extends EntityService<Order> {
 
-    private static final Log log = Logs.get();
-	
-	@At
-	public Object list(@Param("page") int page ,@Param("rows") int rows){
-		if (rows < 1)
-			rows = 10;
-		Pager pager = dao().createPager(page, rows);
-		List<Order> list = dao().query(Order.class, null, pager);
-		Map<String, Object> map = new HashMap<String, Object>();
-		if (pager != null) {
-			pager.setRecordCount(dao().count(Order.class));
-			map.put("pager", pager);
-		}
-		map.put("list", list);
-		return map;
+	private static final Log log = Logs.get();
+
+	private Dao dao;
+
+	/**
+	 * 根据卡号查询卡是否使用过
+	 * 
+	 * @param cardNO
+	 * @return
+	 */
+	@At("/Stocks/queryCardIsUsed")
+	@Ok("json")
+	public boolean queryCardIsUsed(@Param("cardno") String cardNO) {
+
+		boolean rtn = false;
+		Sql sql = Sqls
+				.create("SELECT STATUS FROM ES_CHARGE_STOCKS WHERE CARDNO = @CARDNO");
+		sql.params().set("CARDNO", cardNO);
+		dao.execute(sql);
+		return rtn;
 	}
-	
-	@At
-	public boolean add(@Param("..") Order obj){
-		try{
-			dao().insert(obj);
-			return true;
-		}catch (Throwable e) {
-			if (log.isDebugEnabled())
-				log.debug("E!!",e);
-			return false;
-		}
+
+	/**
+	 * 根据卡号查询卡的使用情况
+	 * 
+	 * @param cardNO
+	 * @return
+	 */
+	@At("/Stocks/queryCardLogs")
+	@Ok("json")
+	public Stocks queryCardLogs(@Param("cardno") String cardNO) {
+
+		Stocks stocks = new Stocks();
+		Sql sql = Sqls
+				.create("SELECT CARDNO, SUBDATE, SUBIP, ACCOUNT, ORDERID, ID FROM ES_CHARGE_LOGS WHERE CARDNO =  @CARDNO");
+		sql.params().set("CARDNO", cardNO);
+		dao.execute(sql);
+		return stocks;
 	}
-	
-	@At
-	public boolean delete(@Param("..") Order obj){
-		try{
-			dao().delete(obj);
-			return true;
-		}catch (Throwable e) {
-			if (log.isDebugEnabled())
-				log.debug("E!!",e);
-			return false;
-		}
+
+	/**
+	 * 根据卡号查询卡的面值和游戏名
+	 * 
+	 * @param cardNO
+	 * @return
+	 */
+	@At("/Stocks/queryCardLogs")
+	@Ok("json")
+	public void queryFaceValues(@Param("cardno") String cardNO) {
+
+		Sql sql = Sqls
+				.create("SELECT FACEVALUES, (SELECT NAME FROM ES_CHARGE_TMP1 WHERE GAMENO = @GAMENO) GAMENAME " +
+						"FROM ES_CHARGE_TMP2 WHERE GAMENO = @GAMENO AND VALUESNO = @VALUESNO");
+		sql.params().set("CARDNO", cardNO);
+		dao.execute(sql);
+		// return ;
 	}
-	
-	@At
-	public boolean update(@Param("..") Order obj){
-		try{
-			dao().update(obj);
-			return true;
-		}catch (Throwable e) {
-			if (log.isDebugEnabled())
-				log.debug("E!!",e);
-			return false;
-		}
+
+	/**
+	 * 充值
+	 * 
+	 * @param cardNO
+	 * @param cardPwd
+	 * @param account
+	 * @param gameId
+	 * @param areaId
+	 * @param serverId
+	 * @param factionId
+	 * @param roleName
+	 * @param email
+	 */
+	public void charge(@Param("cardno") String cardNO,
+			@Param("cardpwd") String cardPwd, @Param("account") String account,
+			@Param("gameid") String gameId, @Param("areaid") String areaId,
+			@Param("serverid") String serverId,
+			@Param("factionid") String factionId,
+			@Param("rolename") String roleName, @Param("email") String email) {
+
 	}
 }
