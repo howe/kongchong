3edc7190f35ee86afce7992483d7bb0a49d07c59
Howe Chiang <howechiang@gmail.com>
2012-08-15 14:01:24 +0800

    初始化

--- src/com/esup/kongchong/module/StocksModule.java
+++ src/com/esup/kongchong/module/StocksModule.java
@@ -1,12 +1,17 @@
 package com.esup.kongchong.module;
 
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.nutz.dao.Dao;
 import org.nutz.dao.Sqls;
 import org.nutz.dao.sql.Sql;
 import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.mvc.annotation.AdaptBy;
 import org.nutz.mvc.annotation.At;
 import org.nutz.mvc.annotation.Ok;
 import org.nutz.mvc.annotation.Param;
+import org.nutz.mvc.upload.TempFile;
 import org.nutz.service.EntityService;
 
 import org.nutz.log.Log;
@@ -15,7 +20,7 @@ import org.nutz.log.Logs;
 import com.esup.kongchong.bean.Order;
 import com.esup.kongchong.bean.Stocks;
 
-@IocBean(fields = { "dao" })
+@IocBean(fields = {"dao"})
 public class StocksModule extends EntityService<Order> {
 
 	private static final Log log = Logs.get();
@@ -89,6 +94,8 @@ public class StocksModule extends EntityService<Order> {
 	 * @param roleName
 	 * @param email
 	 */
+	@At("/Stocks/charge")
+	@Ok("json")
 	public void charge(@Param("cardno") String cardNO,
 			@Param("cardpwd") String cardPwd, @Param("account") String account,
 			@Param("gameid") String gameId, @Param("areaid") String areaId,
@@ -96,5 +103,31 @@ public class StocksModule extends EntityService<Order> {
 			@Param("factionid") String factionId,
 			@Param("rolename") String roleName, @Param("email") String email) {
 
+	}
+	
+	/**
+	 * 验证卡密
+	 * @param cardNO
+	 * @param cardPWD
+	 * @return
+	 */
+	public boolean verifyCard(String cardNO, String cardPWD){
+		
+		Sql sql = Sqls.create("");
+		
+		return true;
+	}
+	
+	/**
+	 * 从execel导入卡密到数据库
+	 * @param tempFile
+	 * @throws InvalidFormatException
+	 * @throws IOException
+	 */
+	@At("/Stocks/importCard")
+	@Ok("json")
+	@AdaptBy(args = { "ioc:upload" })
+	public void importCard(@Param("myfile") TempFile tempFile) throws InvalidFormatException, IOException {
+		
 	}
 }
