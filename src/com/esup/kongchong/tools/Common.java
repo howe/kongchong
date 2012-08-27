617daffc297b07e05b882a8a51105edaeec0c1d9
qi.yang.cn@gmail.com <qi.yang.cn@gmail.com>
2012-08-27 11:29:57 +0800

    update

--- src/com/esup/kongchong/tools/Common.java
+++ src/com/esup/kongchong/tools/Common.java
@@ -3,7 +3,7 @@ package com.esup.kongchong.tools;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Calendar;
+import java.util.Random;
 
 import org.nutz.dao.Sqls;
 import org.nutz.dao.impl.NutDao;
@@ -20,48 +20,76 @@ import org.nutz.ioc.loader.json.JsonLoader;
  */
 public class Common {
 
+	/** 生成随机字符串（数字加字母） */
+	public String generatRandomStrs(int length) {
+
+		char[] numAndLett = ("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ").toCharArray();
+
+		Random random = new Random();
+		StringBuilder randomStrs = new StringBuilder();
+
+		for (int i = 0; i < length; i++) {
+			int tmp = random.nextInt(numAndLett.length);
+			randomStrs.append(numAndLett[tmp]);
+		}
+
+		return randomStrs.toString();
+	}
+
 	/** 通过数据库生成永不重复的序列号 */
-	public String generatSerialNumber() {
+	public String generatSerialNumber(int length) {
 
-		// select t.serialNumber from es_sequence t where t.owner='ChargeCard'
-
-		// update es_sequence t set t.serialNumber=t.serialNumber+1 where
-		// t.owner='ChargeCard'
+		// select t.serialNumber from es_charge_sernum t where
+		// t.serialNumber='111';
 
 		Ioc ioc = new NutIoc(new JsonLoader("conf/ioc/dao.js"));
 		NutDao dao = ioc.get(NutDao.class, "dao");
 
-		Sql sql = Sqls.create("select t.serialNumber from es_sequence t where t.owner='ChargeCard' ");
-		sql.setCallback(new SqlCallback() {
+		String tmp = "init";
+		String strs = null;
+		Sql sql = null;
 
-			@Override
-			public Object invoke(Connection conn, ResultSet rs, Sql sql) throws SQLException {
+		while (tmp != null) {// 查询到记录
 
-				while (rs.next()) {
-					return rs.getString("serialNumber");
+			tmp = generatRandomStrs(length);
+			strs = tmp;
+
+			sql = Sqls.create("select t.serialNumber from es_charge_sernum t where t.serialNumber=@serialNumber ");
+			sql.params().set("serialNumber", tmp);
+
+			sql.setCallback(new SqlCallback() {
+
+				@Override
+				public Object invoke(Connection conn, ResultSet rs, Sql sql) throws SQLException {
+
+					while (rs.next()) {
+						return rs.getString("serialNumber");
+					}
+
+					return null;
 				}
+			});
 
-				return null;
-			}
-		});
+			dao.execute(sql);
+			tmp = sql.getString();
+		}
 
+		// insert into es_charge_sernum values('111',sysdate());
+
+		sql = Sqls.create("insert into es_charge_sernum values(@randomStrs,sysdate()) ");
+		sql.params().set("randomStrs", strs);
 		dao.execute(sql);
 
-		String tmp = sql.getString();
-
-		sql = Sqls.create("update es_sequence t set t.serialNumber=t.serialNumber+1 where t.owner='ChargeCard' ");
-		dao.execute(sql);
-
-		return tmp;
+		return strs;
 	}
 
-	public String generatPassword() {
-		return String.valueOf(Calendar.getInstance().getTimeInMillis());
+	public String generatPassword(int length) {
+		return generatRandomStrs(length);
 	}
 
 	public static void main(String[] args) {
-		System.out.println(new Common().generatSerialNumber());// 打印生成的序列号
-		System.out.println(new Common().generatPassword());// 打印生成的密码
+		System.out.println(new Common().generatSerialNumber(10));// 打印生成的序列号
+		System.out.println(new Common().generatPassword(10));// 打印生成的密码
 	}
 
 }
